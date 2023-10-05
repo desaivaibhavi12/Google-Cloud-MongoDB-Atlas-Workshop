0d8237bd28999b3d0d9af6f2d5ee79454b3e611c
Vaibhavi Desai <117459152+desaivaibhavi12@users.noreply.github.com>
2023-10-05 20:00:14 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  APP_ID: '',
-  GRAPHQL_URI: '',
-  API_KEY: ''
+  APP_ID: 'qwiklabs-gcp-03-9cb9a31700e6',
+  GRAPHQL_URI: 'https://www.cloudskillsboost.google/games/4525/labs/29369',
+  API_KEY: 'RaOyilaiveCXYkNq30iHKq8LDj6kg71FvviTWJauTU5S9Il5zF0ONeDvWkNzrPQ4'
 };
